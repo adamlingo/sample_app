6a744466f8ccdd135be938b4b9efa5f6820da81e
adamlingo <adamlingo@yahoo.com>
2019-09-11 16:00:02 -0500

    add integration test for valid signup for new user

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # check that failed submission re-renders new
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
