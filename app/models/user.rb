9d787bd67b61b8cf737b5b6435867a47c88fb52f
adamlingo <adamlingo@yahoo.com>
2019-09-11 10:41:33 -0500

    add before_save email downcase, uniqueness and presence validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-	validates :name, presence: true
-	validates :email, presence: true
+	# downcase email entries before save to double-ensure uniqueness, same as {self.email = email.downcase}
+	before_save { email.downcase! }
+	validates :name, presence: true, length: {maximum: 50}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 255}, 
+		format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+	has_secure_password
 end
