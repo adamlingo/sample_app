76605a0bcb1e26be06709360f71de3401c9ab11b
adamlingo <adamlingo@yahoo.com>
2019-09-11 11:06:18 -0500

    add validation to password (presence and minimum length), ensure has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255}, 
 		format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-	has_secure_password
+	has_secure_password #works with 'bcrypt gem'
+	validates :password, presence: true, length: { minimum: 6 }
 end
