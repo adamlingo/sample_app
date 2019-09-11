f44c3c613760f753ec9559d11bfcd5b0b16972a2
adamlingo <adamlingo@yahoo.com>
2019-09-11 10:42:29 -0500

    user_test methods for User value validations on name, email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,4 +29,40 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # inspect each element to point to any specific failures
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    # .dup creates duplicate user with same attributes
+    duplicate_user = @user.dup
+    # test for case-sensitivity (currently downcases in User model, so not necessary)
+    # duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    # reload method reloads data from DB, which causes problems without using "before_save in User model"
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
