e5cacafba5cc71a1c0818a2d3b19d1973ac28d7b
adamlingo <adamlingo@yahoo.com>
2019-09-18 11:40:43 -0500

    destroying a session now checks if user is currently logged in before calling 'log_out'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   	# can also write as "if user&.authenticate. . "
   	if user && user.authenticate(params[:session][:password])
   		log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user
   	else
   		# 'flash.now' helps process and disappear message when a new request occurs,
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 end
