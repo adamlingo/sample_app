f28787931b6458acda65d33220478530aac2c1a7
adamlingo <adamlingo@yahoo.com>
2019-09-18 11:47:30 -0500

    add integration test in test_helper to login as particular user

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,22 @@ class ActiveSupport::TestCase
 
   # returns true if a test user is logged in
   def is_logged_in?
-  	!session[:user_id].nil?
+    !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
